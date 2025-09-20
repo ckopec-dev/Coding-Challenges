fa6401abf7f020f4e761d001541911580b9a16d7
Chris Kopec <c_kopec@hotmail.com>
2025-09-20 11:29:53 -0400

    Modified script

--- create.sh
+++ create.sh
@@ -1,50 +1,54 @@
 #!/bin/bash
 
-echo "Processing list."
+# Check if correct number of arguments are provided
+if [[ $# -ne 2 ]]; then
+  echo "Usage: $0 <LANGUAGE_NAME>"
+  exit 1 # Exit with a non-zero status to indicate an error
+fi
 
-# Change to working directory
-cd /mnt/SATA08/intranet/Jobs/Coding-Challenges
+echo "SCRIPT: Entering create."
 
-# To create numbered_lines.txt
-#seq 1 100 > numbered_lines.txt
+PROBLEM_TYPE=$1
+echo "SCRIPT: Setting variable PROBLEM_TYPE: $PROBLEM_TYPE."
+LANGUAGE_NAME=$2
+echo "SCRIPT: Setting variable LANGUAGE_NAME: $LANGUAGE_NAME."
+INPUT_FILE="$PROBLEM_TYPE/$LANGUAGE_NAME/problems.txt"
+echo "SCRIPT: Setting variable INPUT_FILE: $INPUT_FILE."
+WORKING_DIR="/mnt/SATA08/intranet/Jobs/Coding-Challenges"
+echo "SCRIPT: Setting variable WORKING_DIR: $WORKING_DIR."
 
-# Specify the input file
-INPUT_FILE="Euler/Python/problems.txt"
-OUTPUT_PATH="Euler/Python"
+echo "SCRIPT: Changing working directory to $WORKING_DIR."
+cd $WORKING_DIR
 
-# Get first line from input
-LINE=$(head -n 1 $INPUT_FILE)
-echo "SCRIPT: Processing line: $LINE"
+ALGO_NAME=$(head -n 1 $INPUT_FILE)
+echo "SCRIPT: Setting variable ALGO_NAME: $ALGO_NAME."
 
-# Create the prompt
-PROMPT="Solve Eulergit p Problem $LINE using python."
-echo "SCRIPT: Prompt: $PROMPT"
+OUTPUT_FILE="$PROBLEM_TYPE/$LANGUAGE_NAME/${ALGO_NAME// /_}.md"
+echo "SCRIPT: Setting variable OUTPUT_FILE: $OUTPUT_FILE."
 
-# Generate ollama result
-OUTPUT_PATH="$OUTPUT_PATH/$LINE.md"
+PROMPT="Solve Euler problem $ALGO_NAME in the $LANGUAGE_NAME programming language. Use markdown format."
+echo "SCRIPT: Setting variable PROMPT: $PROMPT"
+
 echo "SCRIPT: Using ollama to generate output."
-ollama run qwen3-coder $PROMPT > $OUTPUT_PATH
+ollama run qwen3-coder $PROMPT > $OUTPUT_FILE
+echo "SCRIPT: Created $OUTPUT_FILE."
 
-# Authenticate to github
 echo "SCRIPT: Authenticating to Github."
 gh auth login --hostname github.com --with-token < ../github_token.txt
 
-# Get fresh pull
 echo "SCRIPT: Pulling latest version."
 git pull
 
-# Create a new branch
-BRANCH_NAME="Euler-Python-$LINE"
+BRANCH_NAME=${PROBLEM_TYPE// /_}-${LANGUAGE_NAME// /_}-${ALGO_NAME// /_}
+echo "SCRIPT: Setting variable BRANCH_NAME: $BRANCH_NAME."
 echo "SCRIPT: Creating a new branch named $BRANCH_NAME."
-git checkout -b $BRANCH_NAME
+git checkout -b "$BRANCH_NAME"
 
-# Add updated items
 echo "SCRIPT: Adding files to staging."
 git add .
 
-# Make commit
 echo "SCRIPT: Making commit."
-git commit -m "Added $OUTPUT_PATH"
+git commit -m "Added $OUTPUT_FILE"
 
 # Push the commit
 echo "SCRIPT: Pushing commit."
@@ -52,7 +56,7 @@ git push --set-upstream origin $BRANCH_NAME
 
 # Creating the pull request
 echo "SCRIPT: Creating pull request."
-gh pr create --title "$BRANCH_NAME ready for review" --body "Generating automatically from create.sh."
+gh pr create --title "$BRANCH_NAME ready for review" --body "Generated automatically from create.sh."
 
 # Switch back to main branch
 echo "SCRIPT: Switching back to main."
@@ -62,4 +66,4 @@ git switch main
 echo "SCRIPT: Pruning list."
 sed -i '1d' $INPUT_FILE
 
-echo "SCRIPT: Processing complete."
+echo "SCRIPT: Create complete."
